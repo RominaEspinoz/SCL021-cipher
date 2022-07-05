71c6cc9e00bbf5609d42e388d31197fb3f2a418b
RominaEspinoz <espinoza.hernandez.r@gmail.com>
2022-07-05 18:41:48 -0400

    se eliminaron article de index html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+//Pasos para realizar antes cifrar (Interacciones con el DOM)
+// Al apretar button Crear Mensaje Secreto se abre pagina para codificar mensaje 
+// Al apretar button Descrifrar Mensaje Secreto se abre pagina para decodificar mensaje
+// Guardar mensaje escrito en input "Mensage to code"
+
