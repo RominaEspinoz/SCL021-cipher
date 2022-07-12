47f8b9f878386ccb6556bba68f19f2888e5c87bb
RominaEspinoz <espinoza.hernandez.r@gmail.com>
2022-07-11 23:23:04 -0400

    Se agregaron cuadros para cifrar y descifrar en la misma pagina, se agrego funcion cifrar y funcion descifrar en cipher, se realizo funcionalidad en index.jx

--- src/cipher.js
+++ src/cipher.js
@@ -16,7 +16,25 @@ const cipher = {
       }
     }
     return resultado;
-  }
-};
+  },
 
+  descifrar: function (texto,desplazamiento){
+    let resultado = "";
+    let letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    desplazamiento = (desplazamiento %26 - 26)%26;
+
+    if (texto){
+      for(let i = 0; i < texto.length; i++){
+        if(letras.indexOf(texto[i]) != -1){
+          let posicion = ((letras.indexOf(texto[i])- desplazamiento)%26);
+          resultado += letras[posicion];
+        }
+        else
+        resultado += texto[i];
+      }
+    }
+    return resultado;
+  }
+  }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 window.addEventListener("load", ToCreate, true);
+window.addEventListener("load", ToDecode, true);
+window.addEventListener("load", Todecode, true);
 
 function ToCreate() {
     document.getElementById("messagetocode").addEventListener("keyup", function () {
         this.value = this.value.toUpperCase();
-    }, true);
-    document.getElementById("Tocreate").addEventListener("click", function () {
-        let texto = document.getElementById("messagetocode").value;
+    }, true); // Escucha el momento en que se escribe y transforma lo escrito en mayuscula
+    document.getElementById("Tocreate").addEventListener("click", function () {//Cuando escucha que se hace click en el boton CREAR(id=ToCreate)
+        let texto = document.getElementById("messagetocode").value;// Crea dos variables texto y desplazamiento, ambas vienen del valor ingresado en  incode.html
         let desplazamiento = document.getElementById("movement").value;
-        document.getElementById("textarea").value = cipher.cifrar(texto, desplazamiento);
+        document.getElementById("textarea").value = cipher.cifrar(texto, desplazamiento);// la funcion indica que el valor de textarea sera lo ingresado en las variables luego de haber pasando por el cifrado 
+    }, true);
+}
+//En la misma html(incode.html)
+function ToDecode() {
+    document.getElementById("Todecode").addEventListener("click", function () {//Cuando escucha que se hace click en el boton DESCIFRAR(Id "Todecode")
+        let texto = document.getElementById("textarea").value;// Crea dos variables texto y desplazamiento, ambas vienen del valor ingresado en  decode.html
+        let desplazamiento = document.getElementById("movement").value;
+        document.getElementById("DecodedMessage").value = cipher.descifrar(texto, desplazamiento);// la funcion indica que el valor de textarea sera lo ingresado en las variables luego de haber pasando por el cifrado 
     }, true);
 }
 
+/* Para poner en otra html(decode.html)
+ function Todecode(){
+    document.getElementById("Todecode").addEventListener("click", function(){
+        document.getElementById("mensagetodecode").value = document.getElementById("messagetocode");
+        let texto = document.getElementById("mensagetodecode").value;
+        let desplazamiento = document.getElementById("movement").value;
+        document.getElementById("DecodedMessage").value = cipher.descifrar(texto, desplazamiento);
+    }, true);
+ } */
